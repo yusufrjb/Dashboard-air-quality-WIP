83114e0fab233a1f7f6ccba636a6d0599288eda0
yusufrjb <yusufrajabiyah653@gmail.com>
2026-04-12 14:15:27 +0700

    batal menambahkan requirements.txt 2

--- run_predict.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Cron job to run prediction script every 5 minutes
-# Add to crontab: crontab -e
-# */5 * * * * /path/to/DashboardAQ_v3/run_predict.sh >> /path/to/DashboardAQ_v3/logs/predict.log 2>&1
-
-cd D:\DashboardAQ_v3
-python ml_model/predict_and_store.py
